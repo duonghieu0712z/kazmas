560e45f39d4967654ca988e1ce2a8d015fa52db3
duonghieu0712z <duonghieu0712z@gmail.com>
2026-06-10 23:09:30 +0700

    feat: add activity bar items

--- src/layouts/app-shell/AppActivityBar.vue
+++ src/layouts/app-shell/AppActivityBar.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import type { LucideIcon } from '@lucide/vue';
 
-import { LibraryIcon, ScrollTextIcon } from '@lucide/vue';
+import { ImagesIcon, LibraryIcon, ScrollTextIcon, SettingsIcon, Trash2Icon } from '@lucide/vue';
 
 import { useSidebar } from '@/components/ui/sidebar';
+
+type ItemType = { name: string; icon: LucideIcon };
 
 const ITEMS = [
     {
@@ -14,12 +16,27 @@ const ITEMS = [
         name: 'Wiki',
         icon: LibraryIcon,
     },
+    {
+        name: 'Assets',
+        icon: ImagesIcon,
+    },
+    {
+        name: 'Trash',
+        icon: Trash2Icon,
+    },
 ];
 
-const activeItem = ref<{ name: string; icon: LucideIcon } | null>(null);
+const FOOTERS = [
+    {
+        name: 'Settings',
+        icon: SettingsIcon,
+    },
+];
+
+const activeItem = ref<ItemType | null>(null);
 const { open, setOpen } = useSidebar();
 
-function selectItem(item: { name: string; icon: LucideIcon }) {
+function selectItem(item: ItemType) {
     if (open.value && activeItem.value?.name === item.name) {
         activeItem.value = null;
         setOpen(false);
@@ -65,5 +82,27 @@ onMounted(() => {
                 </SidebarMenuItem>
             </SidebarMenu>
         </SidebarContent>
+
+        <SidebarFooter class="gap-0 p-0">
+            <SidebarMenu class="gap-0">
+                <SidebarMenuItem
+                    v-for="item in FOOTERS"
+                    :key="item.name"
+                    class="flex size-(--sidebar-width-icon) items-center justify-center"
+                >
+                    <SidebarMenuButton
+                        always-show-tooltip
+                        :class="[
+                            'size-8 justify-center p-0 hover:bg-transparent active:bg-transparent',
+                            'hover:[&>svg]:stroke-sidebar-accent-foreground active:[&>svg]:stroke-accent-foreground data-[active=true]:[&>svg]:stroke-accent-foreground [&>svg]:stroke-sidebar-ring',
+                        ]"
+                        :tooltip="item.name"
+                    >
+                        <component :is="item.icon" />
+                        <span class="sr-only">{{ item.name }}</span>
+                    </SidebarMenuButton>
+                </SidebarMenuItem>
+            </SidebarMenu>
+        </SidebarFooter>
     </Sidebar>
 </template>
