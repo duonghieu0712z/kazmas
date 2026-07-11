87149b748ac6225e27f6e7960e0d95aa5a18857d
duonghieu0712z <duonghieu0712z@gmail.com>
2026-07-12 02:01:27 +0700

    feat(nodes): load world node trees on world change

--- src/layouts/app-shell/AppContent.vue
+++ src/layouts/app-shell/AppContent.vue
@@ -1,5 +1,5 @@
 <template>
-    <SidebarInset class="h-full">
+    <SidebarInset>
         <header class="flex h-5 items-center border-b px-2 text-xs">Header</header>
         <div class="flex flex-1 items-center justify-center">Content</div>
     </SidebarInset>
